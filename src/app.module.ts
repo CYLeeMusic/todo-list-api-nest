f3697b218e836b5ba7e944a206c27e9774c72b95
cyleemusic <cyleemusic@gmail.com>
2024-02-27 17:34:28 +0900

    Scaffold: Generate resources for todo feature (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import ormConfig from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig)],
+  imports: [TypeOrmModule.forRoot(ormConfig), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
