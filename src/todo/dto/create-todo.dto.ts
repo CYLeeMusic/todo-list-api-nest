5bcf3443d3f85cb7e8438ed28a475aef96041fa4
cyleemusic <cyleemusic@gmail.com>
2024-02-27 18:45:54 +0900

    WIP: DTO for create-todo (#6)

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,1 +1,19 @@
-export class CreateTodoDto {}
+import { IsBoolean, IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  status: boolean;
+
+  @IsNotEmpty()
+  @IsDate()
+  dueDate: Date;
+}
