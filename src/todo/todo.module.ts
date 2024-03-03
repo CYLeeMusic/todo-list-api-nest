db77008f4f28682f43cae33d79e899cf387f2c8a
cyleemusic <cyleemusic@gmail.com>
2024-03-03 18:39:31 +0900

    WIP: Module for todo (#9)

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './entities/todo.entity';
+import { TodoRepository } from './repositories/todo.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Todo])],
   controllers: [TodoController],
-  providers: [TodoService],
+  providers: [TodoService, TodoRepository],
+  exports: [TypeOrmModule],
 })
 export class TodoModule {}
